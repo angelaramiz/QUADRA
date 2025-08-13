ff31626fbcc59c8c60372c47b19b8c4981b95c96
angelaramiz <144601522+angelaramiz@users.noreply.github.com>
2025-08-12 18:35:53 -0600

    Update run.py

--- app/run.py
+++ app/run.py
@@ -22,7 +22,8 @@ def main():
     
     print("🚀 Iniciando aplicación QUADRA...")
     print("📍 URL: http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     main()
